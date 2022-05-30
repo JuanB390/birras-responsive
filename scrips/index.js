5cc0191c48d5948fa8040f832d6f7612d710b992
JuanB390 <84818571+JuanB390@users.noreply.github.com>
2022-05-29 21:21:06 -0300

    Update index.js

--- scrips/index.js
+++ scrips/index.js
@@ -1,11 +1,42 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const contentPortada = document.querySelector(".content-portada");
+const main = document.querySelector(".main");
 
 
 
-
-navToggle.addEventListener("click" , () => {
+navToggle.addEventListener("click" ,()=>{
     
     navMenu.classList.toggle("nav-menu_visible");
+    todos();
     
 });
+
+
+
+function todos(){
+    funcion1();
+    funcion2();
+    funcion3();
+    funcion4();
+}
+
+function funcion1(){
+   
+    const navVisible = navMenu.classList.contains("nav-menu_visible");
+    if(navVisible){
+        main.style.display = "none";
+      }else{
+        main.style.display = "block";
+      }
+
+}
+function funcion2(){
+    console.log("funcion dos");
+}
+function funcion3(){
+    console.log("funcion tres");
+}
+function funcion4(){
+    console.log("funcion cuatro");
+}
